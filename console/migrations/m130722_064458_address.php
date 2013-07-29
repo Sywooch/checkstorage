9e5aa3a25df596721101c3701d86bec8d3f58523
Philipp Frenzel <philipp@frenzel.net>
2013-07-29 14:46:52 +0200

    add address migrations

--- console/migrations/m130722_064458_address.php
+++ console/migrations/m130722_064458_address.php
@@ -4,12 +4,23 @@ class m130722_064458_address extends \yii\db\Migration
 {
 	public function up()
 	{
+		$this->createTable('tbl_address',array(
+			'id'            	=> 'INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
+			'user_id'			=> 'INTEGER UNSIGNED NOT NULL',			
+			'date_created'		=> 'DATE',
+			'date_deleted'		=> 'DATE DEFAULT NULL',
+			'note'				=> 'TEXT',			
+		),'CHARACTER SET utf8 COLLATE utf8_bin ENGINE = InnoDB;');	
 
+		//ADD FKS
+		$this->addForeignKey('FK_address_user','tbl_address','user_id','tbl_user','id');
 	}
 
 	public function down()
 	{
-		echo "m130722_064458_address cannot be reverted.\n";
-		return false;
+		//drop FKs
+		$this->dropForeignKey('FK_address_user','tbl_address');
+		
+		$this->dropTable('tbl_address');
 	}
 }
